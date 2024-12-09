203fb564b2dfc04fdb954c330cb65560b64669d9
Michael0992 <michel.lutz92@gmail.com>
2024-12-09 08:36:36 +0100

    first commit

---
+++ Kaffemaschine.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaffeemaschine
+{
+	internal class Kaffeemaschine
+	{
+		// Deklaration der Felder die für unsere Kaffemaschinen-Objekte gebraucht werden
+		//Da kein Zugriffsmodifikator savor steht gelten die felder als privat.
+		//Was im Zuge der Datenkapselung auch so sein sollte.
+		int wasserstand; 
+		int bohnenmenge;
+		
+
+		//Die folgenden Felder sind statische Felder. Das bedeutetsie sind für alle Objekte dieser Klasse gleich.
+		//Kein Objekt hat dafür einen individuellen Wert.
+		static int maxWasserstand = 100;
+		static int maxBohnenmenge = 100;
+
+		//Die Eigenschaften unserer Klasse
+		//Diese haben wir aus unseren Feldern erstellt.
+		public int Wasserstand { get => wasserstand; set => wasserstand = value; }
+		public int Bohnenmenge { get => bohnenmenge; set => bohnenmenge = value; }
+		public static int MaxWasserstand { get => maxWasserstand; }
+		public static int MaxBohnenmenge { get => maxBohnenmenge; }
+
+		public Kaffeemaschine(int wasserstand, int bohnenmenge)
+		{
+			this.wasserstand = wasserstand;
+			this.bohnenmenge = bohnenmenge;
+		}
+
+		//Zwei Methoden die jeweils Wasser bzw Bohnen auf das Maximum auffüllen.
+
+		public void WasserAuffuellen()
+		{
+			int auffuelmenge = maxWasserstand - wasserstand;
+			wasserstand = maxWasserstand;
+            Console.WriteLine($"Ihr neuer Wasserstand ist {wasserstand} Einheiten. \nEs wurden dafür {auffuelmenge} Einheiten Wasser gebraucht.");
+			Console.ReadKey();
+		}
+
+		public void BohnenAuffuellen()
+		{
+			int auffuelmenge = maxBohnenmenge - bohnenmenge;
+			bohnenmenge = maxBohnenmenge;
+			Console.WriteLine($"Ihre neuee Bohnenmenge ist {bohnenmenge} Einheiten. \nEs wurden dafür {auffuelmenge} Einheiten Bohnen gebraucht.");
+			Console.ReadKey();
+		}
+
+		private bool Wasserstandspruefung()
+		{
+			if (wasserstand >= 20) return true;
+			else return false;
+		}
+
+		private bool Bohnenmengenpruefung()
+		{
+			if (bohnenmenge >= 2) return true;
+			else return false;
+		}
+
+		//Es folgt die Methode um einen Kaffee zu machen 
+
+		public void KafeeZapfen()
+		{
+			if (Wasserstandspruefung() && Bohnenmengenpruefung())
+			{
+				wasserstand -= 20;
+				bohnenmenge -= 2;
+				Console.WriteLine("Hier bitte Ihr Kaffee");
+			}
+			else if (!Wasserstandspruefung() && Bohnenmengenpruefung()) Console.WriteLine("Bitte Wasser auffüllen.");
+			else if (!Bohnenmengenpruefung() && Wasserstandspruefung()) Console.WriteLine("Bitte Bohnen auffüllen");
+			else Console.WriteLine("Bitte Wasser und bohnen auffüllen");
+			Console.ReadKey();
+		}
+
+		public void KaffeeMenu()
+		{
+			while (true)
+			{
+				Console.Clear();
+				Console.WriteLine($"Senseo Kaffeemaschine 1.0\n\nWasserstand: {wasserstand} Einheiten\tBohnenmenge: {bohnenmenge} Einheiten\n\n");
+                Console.WriteLine("Bitte machen Sie eine Eingabe:\n\t1 - Kaffee machen\n\t2 - Wasser auffüllen\n\t3 - Bohnen auffüllen");
+				Console.Write("Eingabe: ");
+				string eingabe = Console.ReadLine();
+
+				if (eingabe == "1") KafeeZapfen();
+				else if (eingabe == "2") WasserAuffuellen();
+				else if (eingabe == "3") BohnenAuffuellen();
+				else
+				{
+					Console.WriteLine("Falsche eingabe");
+					Console.ReadKey();
+
+				}
+			}
+
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Kaffeemaschine
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//Aufgabe1:
+			//Erstellen Sie die Klasse Kaffeemaschine.
+			//Diese soll über die Felder "wasserstand" und "bohnenMenge" verfügen.
+			//Es soll auch zwei static Felder (googeln Sie bitte was static bedeutet und 
+			//wie es eingesetzt wird) geben namens "maxWasserstand" und "maxBohnenmenge".
+			//Die Klasse soll im Sinne der Datensicherheit gekapselt sein.
+			//Erstellen Sie zwei Methoden, welche jeweils zurückmelden wieviel Wasser und wieviele Bohnen aufgefüllt werden müssen.
+			//Hinweis: Sie werden dann selbst ein Objekt der Klasse Kaffeemaschine erstellen müssen, dem Sie beliebig entsprechende Werte
+			//zuweisen und mit dem Sie dann die Methoden ausführen können.
+			//
+			//Aufgabe 2:
+			//Erstellen Sie eine Methode um einen Kaffee zu machen. Dafür soll eine bestimmte Menge Wasser und eine bestimmte Menge Bohnen
+			//verbraucht werden.
+			//Geben Sie den Kaffee stellvertretend über einen Console.WriteLine aus.
+			//
+			//Aufgabe 3:
+			//Erstellen Sie Methoden um zu prüfen, ob noch ausreichend Wasser und Bohnen für einen Kaffe vorhanden sind.
+			//Diese beiden Methoden sollen dann in der für Aufgabe 2 erstellten Methode sinnvoll aufgerufen werden. 
+			//
+			//Aufgabe 4a: Schreiben Sie Code der es uns ermöglicht wieder und wieder einen Kaffe aus der Maschine zu bekommen.
+			//Aufgabe 4b: Machen Sie diesen Code zu einer Methode der Klasse.
+			//Aufgabe 4c: Verlagern Sie alle bisherigen Methodenaufrufe aus der Programm.cs in die Methode aus 4b.
+			//Diese Methode soll es dann ermöglichen nach Nutzereingabe entweder einen Kaffe zu bekommen oder eine der Auffüllmethoden auszuführen. 
+
+			Kaffeemaschine maschine1 = new Kaffeemaschine(90, 50);
+
+			maschine1.KaffeeMenu();
+
+        }
+	}
+}
